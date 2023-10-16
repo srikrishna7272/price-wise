ef5c42577d36caa97a638e2e6ff9eb0c0bd92ee6
srikrishna7272 <105886453+srikrishna7272@users.noreply.github.com>
2023-10-16 13:59:19 -0700

    built searchbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
+import Searchbar from "@/components/Searchbar";
 import Image from "next/image";
 import React from "react";
 
 const Home = () => {
   return (
     <>
-      <section className="px-6 md:px-20 py-24 border-2">
+      <section className="px-6 md:px-20 py-24">
         <div className="flex max-xl:flex-col gap-16">
           <div className="flex flex-col justify-center">
             <p className="small-text">
@@ -18,7 +19,7 @@ const Home = () => {
             <p className="mt-6 ">
               Powerful, self-serve product and growth analytics to help you convert, engage, and retain more.
             </p>
-            Searchba
+            <Searchbar />
           </div>
           HeroCarousel
         </div>
