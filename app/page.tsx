7f177ea5eef085eb495f97c807025ecedc30e5e6
srikrishna7272 <105886453+srikrishna7272@users.noreply.github.com>
2023-10-16 13:50:44 -0700

    build hero component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,38 @@
+import Image from "next/image";
 import React from "react";
 
 const Home = () => {
-  return <div>home</div>;
+  return (
+    <>
+      <section className="px-6 md:px-20 py-24 border-2">
+        <div className="flex max-xl:flex-col gap-16">
+          <div className="flex flex-col justify-center">
+            <p className="small-text">
+              Smart Shopping Starts Here:
+              <Image src="/assets/icons/arrow-right.svg" alt="arrow-right" width={16} height={16} />
+            </p>
+            <h1 className="head-text">
+              Unleash the power of
+              <span className="text-primary"> Pricewise</span>
+            </h1>
+            <p className="mt-6 ">
+              Powerful, self-serve product and growth analytics to help you convert, engage, and retain more.
+            </p>
+            Searchba
+          </div>
+          HeroCarousel
+        </div>
+      </section>
+      <section className="trending-section">
+        <h2 className="section-text">Trending</h2>
+        <div className="flex flex-wrap gap-x-8 gap-y-16">
+          {["apple Iphone 15", "Book", "sneaker"].map((product) => (
+            <div>{product}</div>
+          ))}
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Home;
