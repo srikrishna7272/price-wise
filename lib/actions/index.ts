867a367bafdb1c35f0736d5b99ff6b3877f978f9
srikrishna7272 <105886453+srikrishna7272@users.noreply.github.com>
2023-10-17 19:04:58 -0700

    connected to Mongoose.

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -1,11 +1,14 @@
 "use server";
 
+import { connectToDB } from "../mongoose";
 import { scrapeAmazonProduct } from "../scrapper";
 
 export async function scrapeAndStoreProduct(productUrl: string) {
   if (!productUrl) return;
   try {
+    connectToDB();
     const scrapedProduct = await scrapeAmazonProduct(productUrl);
+    if (!scrapedProduct) return;
   } catch (error: any) {
     throw new Error(`
     failed to create/update product: ${error.message}`);
